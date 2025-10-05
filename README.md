fff6767caf85b193e27c7fe2ada3a441667453b9
Benjamin Black <benblack769@gmail.com>
2025-10-05 14:38:58 -0600

    Added option to decode image to use new webassembly simd route

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 
 Run  
 ```
-RUSTFLAGS="-C target-feature=+simd128 -C opt-level=2 -C llvm-args=-ffast-math" wasm-pack  build --release --target web
+RUSTFLAGS="-C target-feature=+simd128 -C opt-level=3 -C llvm-args=-ffast-math" wasm-pack  build --release --target web
 ```
 
 Or to build/test faster
 ```
-RUSTFLAGS="-C target-feature=+simd128 -C opt-level=3 -C llvm-args=-ffast-math -C no-vectorize-loops -C no-vectorize-slp" cargo  build --release --target wasm32-unknown-unknown
+RUSTFLAGS="-C target-feature=+simd128 -C opt-level=3 -C llvm-args=-ffast-math  -C llvm-args=-fno-unroll-loops -C no-vectorize-loops -C no-vectorize-slp" cargo  build --release --target wasm32-unknown-unknown
 ```
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,51 @@
-use zune_jpeg::JpegDecoder;
-use zune_core::options::DecoderOptions;
-use zune_core::colorspace::ColorSpace;
 use js_sys;
+use std::io::Cursor;
+use zune_core::colorspace::ColorSpace;
+use zune_core::options::DecoderOptions;
+use zune_jpeg::JpegDecoder;
+
+use image::ImageReader;
+
 use wasm_bindgen::prelude::*;
 
-
 #[wasm_bindgen]
-pub fn decode_img(encoded_js: &js_sys::Uint8Array ) -> Result<js_sys::Uint8Array,String>  {
+pub fn decode_jpeg(encoded_js: &js_sys::Uint8Array) -> Result<js_sys::Uint8Array, String> {
     let encoded = encoded_js.to_vec();
-    let options: DecoderOptions = DecoderOptions::default().jpeg_set_out_colorspace(ColorSpace::RGBA);
+    let options: DecoderOptions =
+        DecoderOptions::default().jpeg_set_out_colorspace(ColorSpace::RGBA);
     let mut decoder = JpegDecoder::new_with_options(&encoded, options);
     let result = decoder.decode();
-    match result{
-        Ok(bytes)=>{
-            let js_arr = js_sys::Uint8Array::new_with_length(bytes.len()as u32);
+    match result {
+        Ok(bytes) => {
+            let js_arr = js_sys::Uint8Array::new_with_length(bytes.len() as u32);
             js_arr.copy_from(&bytes);
             Ok(js_arr)
-        },
-        Err(err)=>Err(format!("Decode error: {err:?}"))
+        }
+        Err(err) => Err(format!("JPEG Decode error: {err:?}")),
+    }
+}
+#[wasm_bindgen]
+pub fn decode_img(
+    encoded_js: &js_sys::Uint8Array,
+    use_fast_jpeg: &js_sys::Boolean,
+) -> Result<js_sys::Uint8Array, String> {
+    if use_fast_jpeg.as_bool().or(Some(false)).unwrap()
+        && encoded_js.length() > 2
+        && encoded_js.get_index(0) == 0xff
+        && encoded_js.get_index(1) == 0xD8
+    {
+        decode_jpeg(encoded_js)
+    } else {
+        let data = encoded_js.to_vec();
+        let img2: Vec<u8> = ImageReader::new(Cursor::new(data))
+            .with_guessed_format()
+            .map_err(|err| format!("Error autodetecting image type: {err:?}"))?
+            .decode()
+            .map_err(|err| format!("Error decoding image: {err:?}"))?
+            .to_rgba8()
+            .into_raw();
+        let js_arr = js_sys::Uint8Array::new_with_length(img2.len() as u32);
+        js_arr.copy_from(&img2);
+        Ok(js_arr)
     }
 }
